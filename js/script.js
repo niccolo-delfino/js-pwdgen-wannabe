61f85206d2f143906406850c8be171ab588c9bd1
Niccolo Delfino <delfino.nicco@libero.it>
2020-12-11 15:55:24 +0100

    Basic structure

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 var nome = prompt('Inserisci qui il tuo nome');
+document.getElementById('nome').innerHTML = nome
 console.log(nome);
 
 var cognome = prompt('Inserisci qui il tuo cognome');
+document.getElementById('cognome').innerHTML = cognome
 console.log(cognome);
 
 var colore = prompt('Inserisci qui il tuo colore preferito');
+document.getElementById('colore').innerHTML = colore
 console.log(colore);
