0c41e91f63bebccb6f33b230c75feb5495f472c2
Niccolo Delfino <delfino.nicco@libero.it>
2020-12-11 15:48:47 +0100

    var + log

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+var nome = prompt('Inserisci qui il tuo nome');
+console.log(nome);
 
+var cognome = prompt('Inserisci qui il tuo cognome');
+console.log(cognome);
+
+var colore = prompt('Inserisci qui il tuo colore preferito');
+console.log(colore);
